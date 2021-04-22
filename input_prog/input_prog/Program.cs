24ca6f60beae9a9614ddca80629d917ff6e78512
Ruslan Sadikov <51824085+ruslans-s@users.noreply.github.com>
2021-04-23 01:43:14 +0300

    Закончил

--- input_prog/input_prog/Program.cs
+++ input_prog/input_prog/Program.cs
@@ -13,30 +13,28 @@ namespace input_prog
             while (true)
             {
                 Console.WriteLine("Введите сообщение");
-                //Ввод выражения для записи в общую память
+
+                //Ввод слова для записи в общую память
                 char[] message = Console.ReadLine().ToCharArray();
                 //Размер введенного сообщения
-                int size = message.Length;
+                int size = message.Length; 
 
-                //Создание участка разделяемой памяти
-                //Первый параметр - название участка, 
-                //второй - длина участка памяти в байтах: тип char  занимает 2 байта 
-                //плюс четыре байта для одного объекта типа Integer
+                //Создание участка разделяемой памяти (Название участка, размер памяти)
                 MemoryMappedFile sharedMemory = MemoryMappedFile.CreateOrOpen("MemoryFile", size * 2 + 4);
+
+                //запись в разделяемую память
                 //Создаем объект для записи в разделяемый участок памяти
                 using (MemoryMappedViewAccessor writer = sharedMemory.CreateViewAccessor(0, size * 2 + 4))
                 {
-                    //запись в разделяемую память
                     //запись размера с нулевого байта в разделяемой памяти
                     writer.Write(0, size);
                     //запись сообщения с четвертого байта в разделяемой памяти
                     writer.WriteArray<char>(4, message, 0, message.Length);
                 }
 
-                Console.WriteLine("Сообщение записано в разделяемую память");
-                Console.WriteLine("Для выхода из программы нажмите любую клавишу");
+                Console.WriteLine("Сообщение записано в разделяемую память"); 
             }
-            Console.ReadLine();
+
         }
     }
 }
--- sort_prog/sort_prog/Program.cs
+++ sort_prog/sort_prog/Program.cs
@@ -1,75 +1,72 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.IO.MemoryMappedFiles;
+using System.Linq;
 using System.Threading;
 
 namespace sort_prog
 {
-   
+
     class Program
     {
-        
+        //Вывод массива в консоль
+        static void outList(List<string> massiveMessage, string line)
+        {
+            Console.WriteLine(line);
+
+            for (int i = 0; i < massiveMessage.Count(); i++)
+            {
+                Console.WriteLine(massiveMessage[i]);
+            }
+        }
+
         static void Main(string[] args)
         {
-            
 
+            //Массив для сообщений из общей памяти
             List<string> massiveMessage = new List<string>();
-            //Получение существующего участка разделяемой памяти
-            //Параметр - название участка
             massiveMessage.Clear();
-            bool enterMessage;
-           
+
             while (true)
             {
-                enterMessage = false;
                 //Массив для сообщения из общей памяти
                 char[] message;
                 //Размер введенного сообщения
                 int size;
 
+                //Получение существующего участка разделяемой памяти (название участка)
                 MemoryMappedFile sharedMemory = MemoryMappedFile.OpenExisting("MemoryFile");
-                //Сначала считываем размер сообщения, чтобы создать массив данного размера
-                //Integer занимает 4 байта, начинается с первого байта, поэтому передаем цифры 0 и 4
+
+                //считываем размер сообщения
                 using (MemoryMappedViewAccessor reader = sharedMemory.CreateViewAccessor(0, 4, MemoryMappedFileAccess.Read))
                 {
                     size = reader.ReadInt32(0);
                 }
 
                 //Считываем сообщение, используя полученный выше размер
-                //Сообщение - это строка или массив объектов char, ка ждый из которых занимает два байта
-                //Поэтому вторым параметром передаем число символов умножив на из размер в байтах плюс
-                //А первый параметр - смещение - 4 байта, которое занимает размер сообщения
                 using (MemoryMappedViewAccessor reader = sharedMemory.CreateViewAccessor(4, size * 2, MemoryMappedFileAccess.Read))
                 {
                     //Массив символов сообщения
                     message = new char[size];
                     reader.ReadArray<char>(0, message, 0, size);
                 }
-                     string stringMessage = new string(message);
-                    if (massiveMessage.IndexOf(stringMessage) == -1)
-                    {
-                        //Переводим массив char в string
-                       
-                        massiveMessage.Add(stringMessage);
+                //Переводим из char[] в string
+                string stringMessage = new string(message);
 
-                        Console.WriteLine("Получено сообщение :");
-                        for(int i = 0; i < massiveMessage.Count(); i++)
-                    {
-                         Console.WriteLine(massiveMessage[i]);
-                    }
-                    Console.WriteLine("Сортируем :");
-
-                    massiveMessage.Sort();
-
-                    for (int i = 0; i < massiveMessage.Count(); i++)
-                    {
-                        Console.WriteLine(massiveMessage[i]);
-                    }
-                }
+                //Провека присутсвия строки в массиве
+                if (massiveMessage.IndexOf(stringMessage) == -1)
+                {
+                    //Добавление строки в массив
+                    massiveMessage.Add(stringMessage);
                     
+                    outList(massiveMessage, "Получено сообщение :");
+                    //Сортировка
+                    massiveMessage.Sort();
+                    
+                    outList(massiveMessage, "Сортируем :");
+
+                }
+
                 Thread.Sleep(200);
             }
         }
